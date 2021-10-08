24998f5b5a2b3bf79dbab1a9338dde87b54672b1
bigbreadguy <50568142+bigbreadguy@users.noreply.github.com>
2021-10-08 14:15:04 +0900

    Update csv2mpii.py
    conversion loop proto

--- csv2mpii.py
+++ csv2mpii.py
@@ -19,22 +19,52 @@ if __name__ == "__main__":
         os.mkdir(result_path)
 
     dir_list = [name for name in os.listdir(target_path) if os.path.isdir(os.path.join(target_path, name))]
-    for dname in dir_list:
+    for dname in tqdm.tqdm(dir_list):
         destination = os.path.join(result_path, dname)
         if not os.path.exists(destination):
             os.mkdir(destination)
 
-    files_grabbed = []
-    for ftype in target_formats:
-        files_grabbed.extend(glob.glob(os.path.join(target_path, ftype)))
-        files_grabbed.extend(glob.glob(os.path.join(target_path, "*",  ftype)))
+        fout = open(os.path.join(destination, "mpii_style.json"), "w", encoding="utf-8")
 
-    for fpath in tqdm.tqdm(files_grabbed):
-        basename = os.path.basename(fpath)[0]
-        dirname = os.path.dirname(fpath)[-1]
+        instances = list()
 
-        fread = open(fpath, "r", encoding="utf-8")
-        content = fread.read()
-        all_lines = content.split("\n")
+        files_grabbed = []
+        for ftype in target_formats:
+            files_grabbed.extend(glob.glob(os.path.join(target_path, dname, ftype)))
+            files_grabbed.extend(glob.glob(os.path.join(target_path, dname, "*",  ftype)))
 
-        fout = open(os.path.join(result_path, dirname, basename + ".json"), "w", encoding="utf-8")
+        for fpath in files_grabbed:
+            basename = os.path.basename(fpath)[0]
+
+            fread = open(fpath, "r", encoding="utf-8")
+            content = fread.read()
+            all_lines = content.split("\n")
+
+            vis = list()
+            joints = list()
+            for line in all_lines:
+                snippets = line.split(",")
+                if "world" in snippets[0]:
+                    pass
+
+                count = 0
+                coords = list()
+                for snip in snippets:
+                    try:
+                        coord = float(snip)
+                        count+=1
+
+                        if count < 3:
+                            coords.append(coord)
+                    except:
+                        pass
+                
+                vis.append(1)
+                joints.append(coords)
+        
+            intance = {"joints_vis" : vis, "joints" : joints,
+                       "image" : basename + ".png", "scale" : 255/300,
+                       "center" : [255/2, 255/2]}
+            instances.append(instance)
+
+            json.dump(instances, fout, ensure_ascii=False)
